dfa76d52a46d092b03293d9f58ce862c9d25c455
Henryk <henryk.umbrasevic@stud.techin.lt>
2024-10-30 15:19:52 +0200

    12th task variable name changed

--- uzd12.js
+++ uzd12.js
@@ -1,9 +1,9 @@
-let result = 0
+let sum = 0
 
 const countInterval = (x, y) => {
     for (let i = x; i <= y; i++)
-        result += i
-    console.log(result);
+        sum += i
+    console.log(sum);
     
 }
 countInterval(1, 5);
