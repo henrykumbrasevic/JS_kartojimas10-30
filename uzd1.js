17143d487502aa9833ae1c4455099573bf6432c5
Henryk <henryk.umbrasevic@stud.techin.lt>
2024-10-30 11:09:37 +0200

    First task done

---
+++ uzd1.js
@@ -0,0 +1,7 @@
+const countDays = (days) => {
+    const hours = days * 24;
+    const minutes = hours * 60;
+
+return `${days} dienos yra ${hours} valandos, arba ${minutes} minuciu`
+}
+console.log(countDays(20));
