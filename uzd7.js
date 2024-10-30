a677fea9b84b5b0414eb746572a7e26f62862015
Henryk <henryk.umbrasevic@stud.techin.lt>
2024-10-30 12:47:42 +0200

    7th task done

---
+++ uzd7.js
@@ -0,0 +1,17 @@
+const pasakykOra = (num) => {
+    switch(num) {
+        case 1:
+            console.log(`Saulėta`);
+            break;
+        case 2:
+            console.log(`lietinga`);
+            break;
+        case 3:
+            console.log(`vėjuota`);
+            break;
+        case 4:
+            console.log(`sniegas`);
+            break;          
+    }
+}
+pasakykOra(4)
