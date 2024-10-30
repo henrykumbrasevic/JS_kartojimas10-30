ea8391373d2683024530a5664ecb700a7b44512d
Henryk <henryk.umbrasevic@stud.techin.lt>
2024-10-30 12:04:34 +0200

    3rd task done

--- uzd1.js
+++ uzd1.js
@@ -2,6 +2,6 @@ const countDays = (days) => {
     const hours = days * 24;
     const minutes = hours * 60;
 
-return `${days} dienos yra ${hours} valandos, arba ${minutes} minuciu`
+return `${days} dienos yra ${hours} valandos, arba ${minutes} minučių`
 }
 console.log(countDays(10));
---
+++ uzd3.js
@@ -0,0 +1,8 @@
+const oddOrEven = (num) => {
+    if (num % 2 == 0)
+        return `skaičius lyginis`
+    else {
+        return `skaičius nelyginis`
+    }
+}
+console.log(oddOrEven(27));
