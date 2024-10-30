7a13c5ede4d023714fe6f84936ba5610c3e6511d
Henryk <henryk.umbrasevic@stud.techin.lt>
2024-10-30 12:57:42 +0200

    8th task done. Adjustments made for 7th task

--- uzd7.js
+++ uzd7.js
@@ -2,16 +2,12 @@ const pasakykOra = (num) => {
   switch (num) {
     case 1:
       return `Saulėta`;
-      break;
     case 2:
       return `lietinga`;
-      break;
     case 3:
       return `vėjuota`;
-      break;
     case 4:
       return `sniegas`;
-      break;
   }
 };
 console.log(pasakykOra(4));
---
+++ uzd8.js
@@ -0,0 +1,21 @@
+const seasons = (num) => {
+  switch (num) {
+    case 12:
+    case 1:
+    case 2:
+      return `žiema`;
+    case 3:
+    case 4:
+    case 5:
+      return `pavasaris`;
+    case 6:
+    case 7:
+    case 8:
+      return "vasara";
+    case 9:
+    case 10:
+    case 11:
+      return `ruduo`;
+  }
+};
+console.log(seasons(11));
